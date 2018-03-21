1bae6b12b719ac66db288119bc9f53a5593ede8b
pouya-moh <other.pouya@gmail.com>
2018-03-21 08:33:00 +0100

    additing stuff for iros18 with EKUT

--- include/RttGazeboGetPose.hpp
+++ include/RttGazeboGetPose.hpp
@@ -11,6 +11,7 @@
 // RTT header files. Might missing some or some be unused
 #include <rtt/RTT.hpp>
 #include <string>
+#include <rst-rt/robot/JointState.hpp>
 
 #include <gazebo/gazebo.hh>
 #include <gazebo/common/common.hh>
@@ -28,10 +29,22 @@ public:
 
 private:
 	// Declare ports and their datatypes    
-    gazebo::physics::ModelPtr model;
-    gazebo::physics::JointPtr joint;
+    gazebo::physics::ModelPtr model, coman;
+//    gazebo::physics::JointPtr joint;
     gazebo::physics::LinkPtr  link;
 //    gazebo::physics::BasePtr  object;
+
+//    RTT::OutputPort<rstrt::geometry::Pose>  pose_out_port;
+//    rstrt::geometry::Pose                   pose_out_data;
+    RTT::OutputPort<rstrt::robot::JointState> position_as_joint_out_port;
+    rstrt::robot::JointState                  position_as_joint_out_data;
+
+    //waist pose as joint angles :|
+    RTT::OutputPort<rstrt::robot::JointState> waist_pose_as_joint_out_port;
+    rstrt::robot::JointState                  waist_pose_as_joint_out_data;
+
+
+    std::string object_name;
 };
 
 #endif // RTTGAZEBOGETPOSE_HPP
--- src/RttGazeboGetPose.cpp
+++ src/RttGazeboGetPose.cpp
@@ -9,16 +9,34 @@
 #include <rtt/Component.hpp>
 
 RttGazeboGetPose::RttGazeboGetPose(std::string const & name) : RTT::TaskContext(name) {
+    addProperty("object_name", this->object_name);
 
+    this->position_as_joint_out_data = rstrt::robot::JointState(3);
+    this->position_as_joint_out_data.angles.setZero();
+    this->position_as_joint_out_port.setName("out_pos_as_angles");
+    this->position_as_joint_out_port.setDataSample(position_as_joint_out_data);
+    ports()->addPort(position_as_joint_out_port).doc("position of object reported as joint angles because!");
+
+    this->waist_pose_as_joint_out_data = rstrt::robot::JointState(7);
+    this->waist_pose_as_joint_out_data.angles.setZero();
+    this->waist_pose_as_joint_out_port.setName("waist_pos_as_angles");
+    this->waist_pose_as_joint_out_port.setDataSample(waist_pose_as_joint_out_data);
+    ports()->addPort(waist_pose_as_joint_out_port).doc("waist_pose of object reported as joint angles because!");
 }
 
 bool RttGazeboGetPose::configureHook() {
-    model = gazebo::physics::get_world()->GetModel("coman");
-    if (model.get() == NULL) {
-        RTT::log(RTT::Info) << "model is null"<<RTT::endlog();
+    model = gazebo::physics::get_world()->GetModel(object_name);
+    coman = gazebo::physics::get_world()->GetModel("coman");
+    if (model.get() == NULL || coman.get()== NULL) {
+        RTT::log(RTT::Info) << "Target object or robot is null"<<RTT::endlog();
         return false;
     } else {        
-        return bool(model);
+        this->link = coman->GetLink("base_link");
+        if (link.get()==NULL) {
+            RTT::log(RTT::Info) << "Cannot find link"<<RTT::endlog();
+        }
+
+        return (bool(model)&&bool(coman)&&bool(link));
     }
 }
 
@@ -29,13 +47,27 @@ bool RttGazeboGetPose::startHook() {
 }
 
 void RttGazeboGetPose::updateHook() {
-    this->link = this->model->GetLink("RShy");
-    if (link.get()==NULL) {
-        RTT::log(RTT::Info) << "Cannot find link"<<RTT::endlog();
-    } else {
-        gazebo::math::Pose pose = link->GetWorldPose();
-        RTT::log(RTT::Info) << pose.pos.x<<"\t"<<pose.pos.y<<"\t"<<pose.pos.z<<RTT::endlog();
-    }
+    gazebo::math::Pose pose_ball = model->GetWorldPose();
+    position_as_joint_out_data.angles(0) = pose_ball.pos.x;
+    position_as_joint_out_data.angles(1) = pose_ball.pos.y;
+    position_as_joint_out_data.angles(2) = pose_ball.pos.z;
+
+    std::cout<<position_as_joint_out_data.angles.transpose()<<std::endl;
+
+    position_as_joint_out_port.write(position_as_joint_out_data);
+
+    gazebo::math::Pose pose_waist = link->GetWorldPose();
+    waist_pose_as_joint_out_data.angles(0) = pose_waist.pos.x;
+    waist_pose_as_joint_out_data.angles(1) = pose_waist.pos.y;
+    waist_pose_as_joint_out_data.angles(2) = pose_waist.pos.z;
+
+    waist_pose_as_joint_out_data.angles(3) = pose_waist.rot.w;
+    waist_pose_as_joint_out_data.angles(4) = pose_waist.rot.x;
+    waist_pose_as_joint_out_data.angles(5) = pose_waist.rot.y;
+    waist_pose_as_joint_out_data.angles(6) = pose_waist.rot.z;
+
+    position_as_joint_out_port.write(position_as_joint_out_data);
+    waist_pose_as_joint_out_port.write(waist_pose_as_joint_out_data);
 }
 
 void RttGazeboGetPose::stopHook() {
